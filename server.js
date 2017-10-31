50c538ce36bcaf4e81718baa574809eef32edd5a
MTrost <mario.trost@bluewin.ch>
2017-10-31 21:03:41 +0100

    Trying out magit

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
-var app = express();
+const app = express();
 
 app.use(express.static(__dirname + '/'));
-//app.set('', 'html');
 
 app.get('/', (req, res) => {
   res.render('index.html');
